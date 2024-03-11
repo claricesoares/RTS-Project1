26ae28581d37fa2436807b531763233e9fd8bcb2
claricesoares <clarice.soares@ee.ufcg.edu.br>
2024-03-11 11:25:59 -0300

    Add semaforo

--- project1.c
+++ project1.c
@@ -1,17 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
 
-#define N_TREMS 7
+#define N_TRENS 7
+#define BUFFER_SIZE 1 //So pode 1 trem de cada vez
+#define N_A1_TRENS 3
+#define N_B1_TRENS 4
 
-sem_t semCru;
+sem_t empty, full, mutex;
+typedef enum{Velocidade, Pessoas, Carga}Prioridade;
 
-struct Trem
-{
+//sem_t semCru;
+
+typedef struct{
     int id;
-    char dir_origem;
+    /*char dir_origem;
     char dir_destino;
-    int prioridade;
-};
+    int prioridade;*/
+    Prioridade prioridade;
+}Trem;
+
+void *cruzamento(void *arg){
+    Trem *trem = (Trem *)arg;
+
+    printf("Trem %d (Prioridade %d) esta se aproximando do cruzamento.",trem->id, trem->prioridade);
+    sleep(rand()%3);
+}
